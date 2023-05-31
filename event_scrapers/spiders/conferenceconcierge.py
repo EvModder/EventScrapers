a84a57a0c4000ab929156de8d5a6501ab559aec4
EvModder <evmodder@gmail.com>
2023-05-31 18:46:42 +0200

    PLACEHOLDER files for other websites to scrape

---
+++ event_scrapers/spiders/conferenceconcierge.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+import logging
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "conferenceconcierge"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def start_requests(self):
+        logging.getLogger('scrapy').propagate = True
+        self.log('Starting requests')
+        urls = [
+            "https://quotes.toscrape.com/page/1/",
+            "https://quotes.toscrape.com/page/2/",
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f"quotes-{page}.html"
+        Path(filename).write_bytes(response.body)
+        self.log(f"Saved file {filename}")
